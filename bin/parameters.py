a4b2292d9469cb09fa8b116344d2b3bc98956d63
Francesco Brandoli <166037780+franzbra@users.noreply.github.com>
2024-11-25 10:14:11 +0100

    Parameter file
    Parameter file used by test_main.py and xml_generator.py

---
+++ bin/parameters.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 17:56:44 2024
+
+@author: Francesco Brandoli
+"""
+# number of cloud and edge nodes to generate
+cloud_nodes = 100
+edge_nodes = 1000
+  
+# Choose the specific region
+selected_regions = [3]
+
+# number of regions to use
+num_regions = len(selected_regions)
+        
+# number of cloud and edge containers to generate
+cloud_containers = 10
+edge_containers = 100
+
+#Proability of a container being in a specific region
+p_e = 0.5
+
+
