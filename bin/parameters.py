73320a62c94444b3d2e2f038e782256901a4b715
Francesco Brandoli <166037780+franzbra@users.noreply.github.com>
2025-05-05 10:10:34 +0200

    Updated parameter files

--- bin/parameters.py
+++ bin/parameters.py
@@ -4,24 +4,20 @@ Created on Thu Nov 21 17:56:44 2024
 
 @author: Francesco Brandoli
 """
-# number of cloud and edge nodes to generate
-cloud_nodes = 100
-edge_nodes = 1000
-  
-# Choose the specific region
-selected_regions = [1,2,4,5]
+# number of cloud and edge nodes to generate per region
+cloud_nodes = 10
+edge_nodes = 15
 
-# number of regions to use
+# List regions allowed
+selected_regions = [1, 2, 3, 4, 5]
+
+# User's region
+user_region = 4
 num_regions = len(selected_regions)
-        
-# number of cloud and edge containers to generate
-cloud_containers = 10
-edge_containers = 100
 
-#Proability of a container being in a specific region
-p_e = 0.5
+# number of cloud and edge containers to generate in total
+cloud_containers = 1
+edge_containers = 1
 
-# Maximum security risk level for pods
-r_max = 0.7
-
-
+# Maximum security risk level allowed for pods
+r_max = 1
